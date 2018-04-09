6d1c6a45b9b28d59df290d5eb5091bac545dc0de
Jean-Roger <jeanroger.orsini@gmail.com>
2018-04-09 21:11:53 +0900

    Webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,8 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /node_modules/,
 				use: {
-					loader: 'babel-loader',
-					option: {
+					loader: '@babel/loader',
+					options: {
 						presets: ['@babel/preset-env', '@babel/preset-react']
 					}
 				}
