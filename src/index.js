f9a5c25b409fd9086d46f65d4d68c76df8a5dc2b
Jean-Roger <jeanroger.orsini@gmail.com>
2018-04-11 19:52:15 +0900

    get JSON array function.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 // TODO
 /**
- * o/ Show subtitles.
+ * x Show subtitles.
+ *
+ *
+ */
+// TODO
+
+/**
+ * o/ get JSON array.
  *
  *
  */
@@ -11,3 +18,18 @@ import ReactDOM from 'react-dom';
 import VideoPlayer from './components';
 
 ReactDOM.render(<VideoPlayer />, document.getElementById('app'));
+
+const get_json_object = () =>
+	new Promise((resolve, reject) => {
+		const xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				resolve(JSON.parse(this.responseText));
+			}
+		};
+		xmlhttp.open('GET', 'subtls/sample.json', true);
+		xmlhttp.send();
+	});
+get_json_object().then(res => {
+	console.log(res);
+});
