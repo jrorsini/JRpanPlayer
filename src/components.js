5ee41b4b4b218543488a75abfb51e2352f6ba160
Jean-Roger <jeanroger.orsini@gmail.com>
2018-04-11 19:13:40 +0900

    Bundled files

--- src/components.js
+++ src/components.js
@@ -1,36 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export class Form extends React.Component {
-	render() {
-		return (
-			<form id="uploadForm" encType="multipart/form-data">
-				<input
-					type="file"
-					name={this.props.s.videoSrc ? 'subtl2upload' : 'video2upload'}
-					onChange={
-						this.props.s.videoSrc
-							? this.props.s.uploadSubtl
-							: this.props.s.uploadVideo
-					}
-				/>
-				<p
-					className={
-						this.props.s.videoSrc ? 'center-align white-text' : 'center-align'
-					}
-				>
-					Upload {this.props.s.videoSrc ? 'subtitles' : 'video'}
-					<br />{' '}
-					<i className="material-icons">
-						{this.props.s.videoSrc ? 'subtitles' : 'play_circle_outline'}
-					</i>
-				</p>
-			</form>
-		);
-	}
-}
-
-export class VideoPlayer extends React.Component {
+export default class VideoPlayer extends React.Component {
 	constructor(props) {
 		super(props);
 	}
@@ -38,7 +9,7 @@ export class VideoPlayer extends React.Component {
 	render() {
 		return (
 			<div className="col s8 offset-s2">
-				<video src={this.props.src} className="col s12" controls />
+				<video src="videos/sample.mp4" controls />
 			</div>
 		);
 	}
