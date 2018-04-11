c6b827e231ec5b91ff851a8bc64e444ef9c9e4e0
Jean-Roger <jeanroger.orsini@gmail.com>
2018-04-11 19:41:54 +0900

    Embeded writing file.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const parser = require('subtitles-parser');
-const srt = fs.readFileSync('./dist/subtls/sample.srt', 'utf8');
 
-const data = parser.fromSrt(srt, true);
-
-fs.writeFileSync('./dist/subtls/sample.json', JSON.stringify(data));
-console.log(data);
+const createJSONedFile = () => {
+	const srt = fs.readFileSync('./dist/subtls/sample.srt', 'utf8');
+	const data = parser.fromSrt(srt, true);
+	fs.writeFileSync('./dist/subtls/sample.json', JSON.stringify(data));
+};
