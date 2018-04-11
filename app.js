62463a5a5718a0d7e0bc8fdbe6f8c14d830389b6
Jean-Roger <jeanroger.orsini@gmail.com>
2018-04-11 19:39:53 +0900

    Created subtitles json file.

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const fs = require('fs');
 const parser = require('subtitles-parser');
-const srt = fs.readdirSync('dist/subtls/sample.srt');
+const srt = fs.readFileSync('./dist/subtls/sample.srt', 'utf8');
+
+const data = parser.fromSrt(srt, true);
+
+fs.writeFileSync('./dist/subtls/sample.json', JSON.stringify(data));
+console.log(data);
