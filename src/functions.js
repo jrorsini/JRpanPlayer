1e3db6694a3aedb78f9120dfbfbe6471e085537a
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-04-19 10:54:23 +0900

    Added part of speech checkers.

--- src/functions.js
+++ src/functions.js
@@ -12,91 +12,7 @@ const part_of_speech = {
 	連体詞: 'abdominalAdj',
 	感動詞: 'interjection',
 	フィラー: 'filler'
-};
-
-/**
- * @param {Object} root element to set text in.
- * @param {String} subtitle to put into tag element.
- * @param {Function} click event handler.
- * @function set inner html to subtitle.
- */
-const showSubtitles = (root, subtitle, handler) => {
-	if (subtitle) {
-		root.innerHTML = subtitle
-			.map(
-				(e, i, arr) =>
-					e.surface_form === '\n' ? `<br/>` : generateMarkup(e, i, arr)
-			)
-			.join('');
-		Object.values(document.getElementsByClassName('jrpan-gloss-tag')).map(
-			tagEl => {
-				tagEl.addEventListener('click', handler);
-			}
-		);
-	}
-};
-
-/**
- * @param {Array} json subtitles list
- * @param {Number} currTime video's current time
- * @return {String} subtitles in time lapse
- */
-const subtitle_in_timeLapse = (json, currTime) => {
-	for (let i = 0; i < json.length; i++) {
-		const e = json[i];
-		if (e.startTime < currTime && e.endTime > currTime) return e.text;
-	}
-	return false;
-};
-
-/**
- * @param {Object} subtitleTag to dig into
- * @return {String} the subtitles into p tag
- */
-const currentSubtitle = subtitleTag => subtitleTag.innerText;
-
-/**
- * @param {Object} word's object
- * @return {String} Marked up word
- */
-const generateMarkup = (word, index, array) => {
-	console.log(word);
-	if (!hasjapaneseCharacter(word.surface_form))
-		return `<div class="jrpan-gloss-tag jrpan-gloss-tag--enable">${
-			word.surface_form
-		}</div>`;
-	if (isKatakana(word.surface_form))
-		return `<div class="jrpan-gloss-tag jrpan-gloss-tag--enable katakana-gloss">${
-			word.surface_form
-		}</div>`;
-	return `<div class="jrpan-gloss-tag jrpan-gloss-tag--enable ${
-		part_of_speech[word.pos]
-	}-gloss ${isProperNoun(word)}">${word.surface_form}</div>`;
-
-	// `<div class="jrpan-gloss-tag jrpan-gloss-tag--enable">${
-	// 		word.surface_form
-	//   }</div>`;
-};
-
-/**
- * @param {Object} word's object
- * @return {String} returns class Name to add for proper nouns.
- */
-const isProperNoun = word =>
-	word.pos_detail_1 === '固有名詞' ? 'jrpan-gloss-tag__proper-noun' : '';
-
-/**
- * @param {Object} word's object
- * @return {String} returns class Name to add for symbols.
- */
-const isSymbol = word =>
-	word.pos_detail_1 === '記号' ? 'jrpan-gloss-tag__symbol' : '';
-
-/**
- * @param {Object} element vidoe element.
- * @return {Number} current time.
- */
-const videoCurrTime = element => Math.round(element.currentTime * 1000);
+}
 
 /*
 |--------------------------------------------------------------------------
@@ -111,7 +27,7 @@ const videoCurrTime = element => Math.round(element.currentTime * 1000);
  */
 const isSelectable = selection =>
 	selection.trim() !== '' &&
-	selection.match(/[\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FAF]/g) !== null;
+	selection.match(/[\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FAF]/g) !== null
 
 /**
  * @param {String} word
@@ -120,18 +36,109 @@ const isSelectable = selection =>
 const isKatakana = word =>
 	word.match(/[\u30A0-\u30FF]/g)
 		? word.match(/[\u30A0-\u30FF]/g).length === word.length
-		: false;
+		: false
+
+/**
+ * @param {Object} word's object
+ * @return {String} returns class Name to add for proper nouns.
+ */
+const isProperNoun = word => (word.pos_detail_1 === '固有名詞' ? true : false)
+
+/**
+ * @param {Object} word's object
+ * @return {String} returns class Name to add for symbols.
+ */
+const isSymbol = word => (word.pos_detail_1 === '記号' ? true : false)
 
 /**
  * @param {String} word
  * @return {Boolean} if the word is only made out of Katakana.
  */
 const hasjapaneseCharacter = word =>
-	word.match(/[\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FAF]/g) ? true : false;
+	word.match(/[\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FAF]/g) ? true : false
+
+/**
+ * @param {Object} word's object
+ * @return {String} Marked up word
+ */
+const generateMarkup = (word, index, array) => {
+	switch (true) {
+		case hasjapaneseCharacter(word.surface_form):
+			return `<div class="jrpan-gloss-tag jrpan-gloss-tag--enable ${
+				part_of_speech[word.pos]
+			}-gloss>${word.surface_form}</div>`
+			break
+		case isKatakana(word.surface_form):
+			return `<div class="jrpan-gloss-tag jrpan-gloss-tag--enable katakana-gloss">${
+				word.surface_form
+			}</div>`
+			break
+		case isProperNoun(word):
+			return `<div class="jrpan-gloss-tag jrpan-gloss-tag--enable jrpan-gloss-tag__proper-noun">${
+				word.surface_form
+			}</div>`
+			break
+		case isSymbol(word):
+			return `<div class="jrpan-gloss-tag jrpan-gloss-tag--enable jrpan-gloss-tag__symbol">${
+				word.surface_form
+			}</div>`
+			break
+		default:
+			return `<div class="jrpan-gloss-tag">${word.surface_form}</div>`
+			break
+	}
+}
+
+/**
+ * @param {Array} json subtitles list
+ * @param {Number} currTime video's current time
+ * @return {String} subtitles in time lapse
+ */
+const subtitle_in_timeLapse = (json, currTime) => {
+	for (let i = 0; i < json.length; i++) {
+		const e = json[i]
+		if (e.startTime < currTime && e.endTime > currTime) return e.text
+	}
+	return false
+}
+
+/**
+ * @param {Object} subtitleTag to dig into
+ * @return {String} the subtitles into p tag
+ */
+const currentSubtitle = subtitleTag => subtitleTag.innerText
+
+/**
+ * @param {Object} element vidoe element.
+ * @return {Number} current time.
+ */
+const videoCurrTime = element => Math.round(element.currentTime * 1000)
+
+/**
+ * @param {Object} root element to set text in.
+ * @param {String} subtitle to put into tag element.
+ * @param {Function} click event handler.
+ * @function set inner html to subtitle.
+ */
+const showSubtitles = (root, subtitle, handler) => {
+	if (subtitle) {
+		root.innerHTML = subtitle
+			.map(
+				(e, i, arr) =>
+					e.surface_form === '\n' ? `<br/>` : generateMarkup(e, i, arr)
+			)
+			.join('')
+		Object.values(document.getElementsByClassName('jrpan-gloss-tag')).map(
+			tagEl => {
+				tagEl.addEventListener('click', handler)
+			}
+		)
+	}
+}
 
 module.exports = {
 	subtitle_in_timeLapse,
 	videoCurrTime,
 	showSubtitles,
 	currentSubtitle
-};
+}
